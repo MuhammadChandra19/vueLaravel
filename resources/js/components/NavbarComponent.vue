b14eb8cd5683e79f0a2f99d50fb11135312c309f
Muhammad chandra zulfikar <Chanjull19@gmail.com>
2020-06-05 19:56:47 +0700

    first commit

---
+++ resources/js/components/NavbarComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="border-top border-bottom">
+    <div class="container">
+      <nav class="navbar navbar-expand-sm justify-content-between">
+        <a class="navbar-brand" href="#">
+          <img src="/img/logo.png" alt="logo" class="w-75" />
+        </a>
+        <div class="collapse navbar-collapse float-right">
+          <div class="navbar-nav ml-auto">
+            <a class="nav-item nav-link" href="#">Home</a>
+            <a class="nav-item nav-link" href="#">Features</a>
+            <a class="nav-item nav-link" href="#">Pricing</a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
